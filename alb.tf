e11c6769d26e4764cd1928b851b8eb0d2196509c
srjgrg <srjlamichane@gmail.com>
2020-02-21 19:17:14 -0500

    add

--- alb.tf
+++ alb.tf
@@ -4,9 +4,7 @@ resource "aws_lb" "default" {
   load_balancer_type = "application"
   security_groups    = ["${aws_security_group.default_lb.id}"]
   subnets            = ["subnet-122b865f", "subnet-122b865f"]
-
   enable_deletion_protection = true
-
   tags = {
     Environment = "dev"
   }
--- lb-sgs.tf
+++ lb-sgs.tf
@@ -1,18 +1,17 @@
-resource "aws_security_group" "default" {
+resource "aws_security_group" "default_lb" {
   name        = "terraform-springboot-helloapp-lb"
   description = "Allow traffic"
   vpc_id      = "vpc-62567f18"
-
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
- egress {
+  egress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
- }
+} 
